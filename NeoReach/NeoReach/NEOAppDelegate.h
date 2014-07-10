016d4e6832b7755e97b347f792fef3d5ce412144
Sam Crognale <sam.crognale@gmail.com>
2014-07-10 16:18:32 -0700

    added empty NeoReach XCode project

---
+++ NeoReach/NeoReach/NEOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NEOAppDelegate.h
+//  NeoReach
+//
+//  Created by Sam Crognale on 7/10/14.
+//  Copyright (c) 2014 NeoReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NEOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NeoReach/NeoReach/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NeoReach
+//
+//  Created by Sam Crognale on 7/10/14.
+//  Copyright (c) 2014 NeoReach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NEOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NEOAppDelegate class]));
+    }
+}
