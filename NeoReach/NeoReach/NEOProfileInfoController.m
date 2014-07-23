7a17614c03fcb51ed0d908109c6bc16c44a1c77b
Mathieu Rolfo <rolfom01@stanford.edu>
2014-07-23 16:32:27 -0400

    Continued UI Changes

---
+++ NeoReach/NeoReach/NEOProfileInfoController.m
@@ -0,0 +1,38 @@
+//
+//  NEOProfileInfoController.m
+//  NeoReach
+//
+//  Created by Mathieu Rolfo on 7/23/14.
+//  Copyright (c) 2014 NeoReach. All rights reserved.
+//
+
+#import "NEOProfileInfoController.h"
+
+@interface NEOProfileInfoController ()
+
+@end
+
+@implementation NEOProfileInfoController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
