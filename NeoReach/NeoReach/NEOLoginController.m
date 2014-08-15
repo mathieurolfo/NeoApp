6406ce7fba9d9c857635bdd8cec1587e409f6c47
Mathieu Rolfo <rolfom01@stanford.edu>
2014-08-15 11:13:21 -0700

    Made status bar dark when the webview loads.

--- NeoReach/NeoReach/NEOLoginController.m
+++ NeoReach/NeoReach/NEOLoginController.m
@@ -207,6 +207,7 @@
                //the user is redirected to the actual 'enter login info' page
                [redirectAddress hasPrefix:@"https://m.facebook.com/v1.0/dialog/oauth?redirect"] ||
                [redirectAddress hasPrefix:@"https://m.facebook.com/dialog/oauth"]) { //display login screen
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
         webView.hidden = NO;
         self.splashImage.hidden = YES;
     }
