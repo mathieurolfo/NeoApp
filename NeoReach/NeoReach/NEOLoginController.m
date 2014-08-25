8c7b5313dbc75164530bc2092e97e77b609d3a7f
Mathieu Rolfo <rolfom01@stanford.edu>
2014-08-25 13:33:18 -0400

    changed url string to allow for fb/safari authentication.

--- NeoReach/NeoReach/NEOLoginController.m
+++ NeoReach/NeoReach/NEOLoginController.m
@@ -68,6 +68,8 @@
 - (IBAction)logInPressed:(id)sender {
     NEOAppDelegate *delegate = [[UIApplication sharedApplication] delegate];
 
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:10.0 target:self selector:@selector(cancelRequest) userInfo:nil repeats:NO];
+    NSLog(@"%u", FBSession.activeSession.state);
     if (FBSession.activeSession.state == FBSessionStateOpen ||
         FBSession.activeSession.state == FBSessionStateOpenTokenExtended) {
         [FBSession.activeSession closeAndClearTokenInformation];
@@ -75,53 +77,16 @@
     } else {
         [FBSession openActiveSessionWithReadPermissions:@[@"public_profile"] allowLoginUI:YES completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
             NSLog(@"session opened, token is %@", session.accessTokenData);
-            //session.accessTokenData
-            //NEOAppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
-            //[appDelegate sessionStateChanged:session state:state error:error];
             
             NSString *token = [NSString stringWithFormat:@"%@", session.accessTokenData];
+            [self displayActivityIndicator];
             [delegate.user pullHeadersFromToken:token];
-            //dispatch_async(dispatch_get_main_queue(), ^{
-             //   [delegate.user pullProfileInfo];
-            //});
+
         }];
-        
     }
-    
-    /*
-    [self.loginButton setEnabled:NO];
-    
-    NEOAppDelegate *delegate = [[UIApplication sharedApplication] delegate];
-    NSLog(@"Login pressed");
-    NSLog(@"%@", delegate.sessionConfig.HTTPAdditionalHeaders);
-    if (!delegate.xAuth) {
-        NSLog(@"laod webviw");
-        [self loadWebView];
-    } else {
-    
-        [delegate.user pullProfileInfo]; //this attempts to use the saved header information. if it works, we go straight to the dashboard; if it doesn't, the "header invalid" notification is issued and the selector loadWebView is called.
-    }
-    */
 }
 
 //called if no headers exist
--(void)loadWebView
-{
-    UIWebView *webView = [self configureWebView];
-    self.count = self.count +1;
-    NSLog(@"Webview configured for %ith time", self.count);
-    NSURL *loginURL = [NSURL URLWithString:self.loginAddress];
-    NSLog(@"%@", loginURL);
-    NSURLRequest *request = [NSURLRequest requestWithURL:loginURL];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSLog(@"Starting a new web request");
-        [webView loadRequest:request];
-        NSLog(@"Loading request");
-        [self displayActivityIndicator];
-    });
-
-}
 
 -(void)createDashboard
 {
@@ -143,61 +108,6 @@
 
 
 
-#pragma mark - WebView Methods
-
--(UIWebView *)configureWebView
-{
-    NEOAppDelegate *delegate = (NEOAppDelegate *)[[UIApplication sharedApplication] delegate];
-    
-    //NOTE: hardcoded web view to make status bar visible
-    delegate.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height)];
-    
-    delegate.webView.scrollView.scrollEnabled = NO;
-    delegate.webView.delegate = self;
-    delegate.webView.scalesPageToFit = YES;
-    
-    [delegate.window addSubview:delegate.webView];
-    delegate.webView.hidden = YES;
-    
-    
-    self.loginAddress = @"https://api.neoreach.com/auth/facebook";
-    return delegate.webView;
-}
-
--(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
-    
-    NSURL *redirect = [request mainDocumentURL];
-    NSString *redirectAddress = [redirect absoluteString];
-    //mediocre fix for white landing page: the URL appears twice at the last page so if the previous one is the same, display the web view.
-    self.prevRedirectAddress = self.currRedirectAddress;
-    self.currRedirectAddress = redirectAddress;
-    
-    NSLog(@"%@", self.prevRedirectAddress);
-    
-    if ([redirectAddress hasPrefix:@"https://api.neoreach.com/auth/facebook/callback"]) { //terminate request early to get Auth Header
-        self.redirectURL = redirect;
-        NSLog(@"Get auth header");
-        [self getAuthHeader];
-        
-        return NO;
-    } else if (
-            //the login address appears twice (loading quirk?)
-               ([redirectAddress hasPrefix:@"https://m.facebook.com/login"] &&
-               [self.currRedirectAddress isEqualToString:self.prevRedirectAddress]) ||
-               
-               //the user is redirected to the actual 'enter login info' page
-               [redirectAddress hasPrefix:@"https://m.facebook.com/v1.0/dialog/oauth?redirect"] ||
-               [redirectAddress hasPrefix:@"https://m.facebook.com/dialog/oauth"] ||
-               [redirectAddress hasPrefix:@"https://www.facebook.com/dialog/oauth"]) { //
-        
-        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:YES];
-        [self setNeedsStatusBarAppearanceUpdate];
-        webView.hidden = NO;
-        self.splashImage.hidden = YES;
-    }
-    return YES;
-}
 
 -(void)getAuthHeader
 {
@@ -248,7 +158,7 @@
     
 }
 
--(void)cancelWeb
+-(void)cancelRequest
 {
     [self.loginIndicator stopAnimating];
     NEOAppDelegate *delegate = [[UIApplication sharedApplication] delegate];
@@ -257,12 +167,6 @@
     self.logInOutInfoLabel.text = @"Login timed out";
     NSLog(@"Login timed out");
 }
-
--(void)webViewDidStartLoad:(UIWebView *)webView
-{
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:10.0 target:self selector:@selector(cancelWeb) userInfo:nil repeats:NO];
-}
-
 
 
 
